53772725ed408af3136c058f0ca087f8d24d0f5b
wael-zoaiter <wael.syr.95@gmail.com>
2019-02-02 23:45:11 +0200

    first deploy

---
+++ docs/precache-manifest.60ef8ec27719db866a233a62ea15716c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9735488eefa2bfef5f1b",
+    "url": "/static/js/main.9735488e.chunk.js"
+  },
+  {
+    "revision": "041c9822a1c00187f6f3",
+    "url": "/static/js/1.041c9822.chunk.js"
+  },
+  {
+    "revision": "9735488eefa2bfef5f1b",
+    "url": "/static/css/main.cdd1fe0f.chunk.css"
+  },
+  {
+    "revision": "af88079dea19ac63bfc1062db54b7f4d",
+    "url": "/index.html"
+  }
+];
