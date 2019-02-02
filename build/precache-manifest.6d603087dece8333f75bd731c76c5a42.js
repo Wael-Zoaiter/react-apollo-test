dd3097710a49825de700f42f3371a6b0069336dc
wael-zoaiter <wael.syr.95@gmail.com>
2019-02-02 23:52:06 +0200

    deploy 3

---
+++ build/precache-manifest.6d603087dece8333f75bd731c76c5a42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acc90147bd90d443a3cc",
+    "url": "/react-apollo-test/static/js/runtime~main.acc90147.js"
+  },
+  {
+    "revision": "b6cf38101860fee6a3bc",
+    "url": "/react-apollo-test/static/js/main.b6cf3810.chunk.js"
+  },
+  {
+    "revision": "041c9822a1c00187f6f3",
+    "url": "/react-apollo-test/static/js/1.041c9822.chunk.js"
+  },
+  {
+    "revision": "b6cf38101860fee6a3bc",
+    "url": "/react-apollo-test/static/css/main.bffac5fa.chunk.css"
+  },
+  {
+    "revision": "18007b6a2846a1c21cb842ebe55f272d",
+    "url": "/react-apollo-test/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60ef8ec27719db866a233a62ea15716c.js"
+  "/react-apollo-test/precache-manifest.6d603087dece8333f75bd731c76c5a42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-apollo-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
