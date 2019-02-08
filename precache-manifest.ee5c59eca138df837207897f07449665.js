258a06cc5f7c60fb01393a79eeebfec47b59abc5
wael-zoaiter <wael.syr.95@gmail.com>
2019-02-08 16:34:31 +0200

    Updates

--- precache-manifest.ee5c59eca138df837207897f07449665.js
+++ precache-manifest.ee5c59eca138df837207897f07449665.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-apollo-test/static/js/runtime~main.acc90147.js"
   },
   {
-    "revision": "6ed76320e5363c435b2e",
-    "url": "/react-apollo-test/static/js/main.6ed76320.chunk.js"
+    "revision": "87c549134522f084a529",
+    "url": "/react-apollo-test/static/js/main.87c54913.chunk.js"
   },
   {
     "revision": "93177c7c57c3c5e2f955",
     "url": "/react-apollo-test/static/js/1.93177c7c.chunk.js"
   },
   {
-    "revision": "6ed76320e5363c435b2e",
+    "revision": "87c549134522f084a529",
     "url": "/react-apollo-test/static/css/main.adaa7a2d.chunk.css"
   },
   {
-    "revision": "508f4e462a3d674d16e68adfa6359134",
+    "revision": "9f132ba67c15ab2228fe628e64e8cd3a",
     "url": "/react-apollo-test/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-apollo-test/precache-manifest.54ecacd349a383b9ba99cfd0b4e04686.js"
+  "/react-apollo-test/precache-manifest.ee5c59eca138df837207897f07449665.js"
 );
 
 workbox.clientsClaim();
